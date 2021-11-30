54c23521d4d73c01a443be4d472ebef53774f6c3
javierorozco2 <eorozco8@ucol.mx>
2021-11-29 23:39:30 -0600

    introsScreen

--- app/src/main/java/com/leon/prueb1/Login.java
+++ app/src/main/java/com/leon/prueb1/Login.java
@@ -2,12 +2,18 @@ package com.leon.prueb1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.leon.prueb1.includes.MyToolbar;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText email, password;
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,33 @@ public class Login extends AppCompatActivity {
 
         //MOSTRAR TOOLBAR
         MyToolbar.show(this,"Login",true);
+
+        email = (TextInputEditText) findViewById(R.id.email);
+        password = (TextInputEditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.MainBtnLogin);
+        register = (Button) findViewById(R.id.MainBtnRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,Register.class);
+                startActivity(i);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+
+    }
+
+    private void login() {
+        String sEmail = email.getText().toString();
+        String sPass = password.getText().toString();
+
     }
 }
