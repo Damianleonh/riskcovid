66395f9c8fd4bfb0a25cd59efe1547d45b0c963b
damian <rleon2@ucol.mx>
2021-12-01 07:50:58 -0600

    pt6

--- app/src/main/java/com/leon/prueb1/MainActivity.java
+++ app/src/main/java/com/leon/prueb1/MainActivity.java
@@ -1,29 +1,72 @@
 package com.leon.prueb1;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.leon.prueb1.includes.MyToolbar;
+import com.google.android.gms.location.LocationRequest;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
+    private com.google.android.gms.location.LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocation;
+    private final static int LOCATION_REQUEST_CODE = 1;
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for (Location location : locationResult.getLocations()) {
+                if (getApplicationContext() != null) {
+                    //Obtener localizacion tiempo real
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder().target(new LatLng(location.getLatitude(), location.getLongitude())).zoom(15f).build()
+                    ));
+                    double lat = location.getLatitude();
+                    double longi = location.getLongitude();
+                    Toast.makeText(MainActivity.this, "LATITUD: " + lat + "\nLONGITUD: " + longi, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
 
     //Resources
-    Button mMainBtnLogin,mMainBtnRegister, mMainBtnCalculadora;
+    Button mMainBtnLogin, mMainBtnRegister, mMainBtnCalculadora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyToolbar.show(this, "Risk Covid", false);
+
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
 
         //++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -36,11 +79,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMainBtnRegister = findViewById(R.id.MainBtnRegister);
         mMainBtnCalculadora = findViewById(R.id.MainBtnCalculadora);
 
+
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
         //BOTONES DE CAMBIO DE PANTALLA
         mMainBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Register.class);
+                Intent intent = new Intent(MainActivity.this, Register.class);
                 startActivity(intent);
             }
         });
@@ -48,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMainBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Login.class);
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);
             }
         });
@@ -56,8 +103,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMainBtnCalculadora.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ActivityCalculadora.class);
+                Intent intent = new Intent(MainActivity.this, ActivityCalculadora.class);
                 startActivity(intent);
+
             }
         });
     }
@@ -66,6 +114,61 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(5);
+        startLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+                }
+            }
+        }
+    }
+
+    private void startLocation(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+            }else {
+                checkLocationPermissions();
+            }
+        }else {
+            mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+        }
+    }
+
+    private void  checkLocationPermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION));
+            new AlertDialog.Builder(this)
+                    .setTitle("PROPORCIONA PERMISOS DE LOCALIZACION")
+                    .setMessage("Se requieren permisos de localizacion")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                        }
+                    }).create().show();
+        }
+        else{
+            ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+
+        }
+
 
     }
 }
